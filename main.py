e33350ecc83a71a400586d387217a170f211df02
Kelly Cantrell <knc97a@gmail.com>
2025-09-23 17:04:40 -0500

    Added screen feature.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import sys
 from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
@@ -9,9 +10,21 @@ from constants import (
 )
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    try:
+        print("Starting Asteroids!")
+        print(f"Screen width: {SCREEN_WIDTH}")
+        print(f"Screen height: {SCREEN_HEIGHT}")
+        pygame.init()
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+            screen.fill("black")
+            pygame.display.flip()
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected, exiting gracefully.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
