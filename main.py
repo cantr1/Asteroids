b07b392598e8463f28b847895dc7faf119ea56bb
Kelly Cantrell <knc97a@gmail.com>
2025-09-23 17:11:25 -0500

    Added time and delta feature.

--- main.py
+++ main.py
@@ -11,17 +11,32 @@ from constants import (
 
 def main():
     try:
+        # Print some variables
         print("Starting Asteroids!")
         print(f"Screen width: {SCREEN_WIDTH}")
         print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # Initialize Pygame
         pygame.init()
+
+        # Setup clock
+        clock = pygame.time.Clock()
+        dt = 0
+    
+
+        # Create a screen
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+        # Infinite while-loop to persist the screen
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
             screen.fill("black")
             pygame.display.flip()
+            delta = clock.tick(60)
+            dt = delta / 1000
+
     except KeyboardInterrupt:
         print("\nKeyboardInterrupt detected, exiting gracefully.")
         sys.exit(0)
