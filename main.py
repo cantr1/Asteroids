b67a545223258d113aec6068d9cf76f0c5029742
Kelly Cantrell <knc97a@gmail.com>
2025-09-23 17:19:24 -0500

    Added player object.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 import sys
+from player import Player
 from constants import (
     SCREEN_WIDTH,
     SCREEN_HEIGHT,
     ASTEROID_MIN_RADIUS,
     ASTEROID_MAX_RADIUS,
     ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE
+    ASTEROID_SPAWN_RATE,
+    PLAYER_RADIUS
 )
 
 def main():
@@ -27,12 +29,16 @@ def main():
         # Create a screen
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+        # Create the player object
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
         # Infinite while-loop to persist the screen
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return
             screen.fill("black")
+            player.draw(screen)
             pygame.display.flip()
             delta = clock.tick(60)
             dt = delta / 1000
